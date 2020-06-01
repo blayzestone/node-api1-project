8e23b06a20ed1126b5184e3abadf5a9e7ccc859b
Blayze <blayzestone@protonmail.com>
2020-06-01 13:08:23 -0700

    create post and get requests for /api/users
    * GET /api/users - returns all users
    * POST /api/users - creates a new user and returns it

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
 const express = require("express");
 
 const server = express();
+
+const port = 8000;
+
+server.use(express.json());
+
+let users = [
+  {
+    id: 0,
+    name: "John Doe",
+    bio: "foobar",
+  },
+];
+
+server.post("/api/users", (req, res) => {
+  const user = {
+    ...req.body,
+    id: users.length,
+  };
+  users.push(user);
+  res.status(200).json(user);
+});
+
+server.get("/api/users", (req, res) => {
+  res.status(200).json(users);
+});
+
+server.get("/api/users/:id", (req, res) => {});
+
+server.delete("/api/users/:id", (req, res) => {});
+
+server.patch("/api/users/:id", (req, res) => {});
+
+server.listen(port, () =>
+  console.log(`\n Server is running on port ${port} \n`)
+);
